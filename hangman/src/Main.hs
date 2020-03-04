21d8465ea07d0145b54778268678b861c63c61c6
Francisco J.H. Heras <fjhheras@gmail.com>
2020-03-04 21:48:22 +0000

    First

---
+++ hangman/src/Main.hs
@@ -0,0 +1,43 @@
+module Main where
+
+import           Control.Monad (forever)
+import           Data.Char     (toLower)
+import           Data.List     (intersperse)
+import           Data.Maybe    (isJust)
+import           System.Exit   (exitSuccess)
+import           System.Random (randomRIO)
+
+type WordList = [String]
+
+allWords :: IO WordList
+allWords = do
+  dict <- readFile "data/dict.txt"
+  return (lines dict)
+
+minWordLenght :: Int
+minWordLenght = 5
+
+maxWordLenght :: Int
+maxWordLenght = 9
+
+gameWords :: IO WordList
+gameWords = do
+  aw <- allWords
+  return (filter gameLength aw)
+  where gameLength w =
+          let l = length (w :: String)
+          in l >= minWordLenght && l < maxWordLenght
+
+randomWord :: WordList -> IO String
+randomWord wl = do
+  randomIndex <- randomRIO (0, length wl - 1)
+  return $ wl !! randomIndex
+
+randomWord' :: IO String
+randomWord' = gameWords >>= randomWord
+
+main :: IO ()
+main = do
+  putStrLn "hello world"
+  word <- randomWord'
+  putStrLn word
