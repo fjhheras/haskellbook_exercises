ec339f844779878a0bd62f5f6a304d1591e58997
F.J.H Heras <fjhheras@none>
2020-07-11 18:52:39 +0100

    Ch17 combinations

---
+++ chapter17/combinations.hs
@@ -0,0 +1,12 @@
+import Control.Applicative (liftA3)
+
+stops :: String
+stops = "pbtdkg"
+
+vowels :: String
+vowels = "aeiou"
+
+combos :: [a] -> [b] -> [c] -> [(a, b, c)]
+combos = liftA3 (,,)
+
+main = print $ combos stops vowels stops
