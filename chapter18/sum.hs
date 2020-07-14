b4392746eef81a599f972be027b5b1c915938d8d
F.J.H Heras <fjhheras@gmail.com>
2020-07-14 21:03:32 +0100

    Adding bad monad

--- chapter18/sum.hs
+++ chapter18/sum.hs
@@ -1,13 +1,9 @@
-module Main where
-
 import           Test.QuickCheck
 import           Test.QuickCheck.Checkers
 import           Test.QuickCheck.Classes
 
-data Sum a b = First a | Second b deriving (Eq, Show)
 
---instance Eq a => EqProp (Sum a b) where
---  (=-=) = eq
+data Sum a b = First a | Second b deriving (Eq, Show)
 
 instance Functor (Sum a) where
         fmap _ (First  x) = First x
@@ -24,8 +20,18 @@ instance Monad (Sum a) where
         (>>=) (First  x) f = First x
         (>>=) (Second x) f = f x
 
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Sum a b) where
+        arbitrary = oneof [First <$> arbitrary, Second <$> arbitrary]
+
+instance (Eq a, Eq b) => EqProp (Sum a b) where
+        (=-=) = eq
+
 main :: IO ()
-main = do 
-  let triggerSum :: (Sum Int)  (Int, String, Int)
-      triggerSum = undefined
-  quickBatch $ monad triggerSum -- UNFINISHED. This fails
+main = do
+        let     triggerSum :: (Sum Int) (Int, String, Int)
+                triggerSum = undefined
+        verboseBatch $ functor triggerSum
+        verboseBatch $ applicative triggerSum
+        verboseBatch $ monad triggerSum
+
+
