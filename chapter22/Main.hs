3787521a0508c55fb8ec63a67158c288d9d737d9
F.J.H Heras <fjhheras@gmail.com>
2020-08-09 20:37:22 +0100

    Starting ex of ch 22

--- chapter22/Main.hs
+++ chapter22/Main.hs
@@ -1,6 +1,10 @@
+{-# LANGUAGE InstanceSigs #-}
 module Main where
 
 import Data.Char
+
+newtype Reader r a =
+  Reader { runReader :: r -> a }
 
 cap :: [Char] -> [Char]
 cap xs = map toUpper xs
@@ -25,6 +29,32 @@ tuppledMdo = do
 
 tuppledM :: [Char] -> ([Char], [Char])
 tuppledM = cap >>= (\x -> rev >>= (\y -> return (x, y))) 
+
+--
+
+myLiftA2 :: Applicative f => (a -> b -> c) -> f a -> f b -> f c
+myLiftA2 f x y = f <$> x <*> y 
+
+asks :: (r -> a) -> Reader r a
+asks f = Reader f
+
+--
+instance Functor (Reader r) where
+  fmap f (Reader g) = Reader (f . g)
+
+instance Applicative (Reader r) where
+  pure :: a -> Reader r a -- This would not work without InstanceSigs
+  pure a = Reader $ const a
+  (<*>) :: Reader r (a -> b) -> Reader r a -> Reader r b
+  (Reader rab) <*> (Reader ra) = Reader $ \r -> rab r (ra r) 
+
+instance Monad (Reader r) where
+  return = pure
+  (>>=) :: Reader r a -> (a -> Reader r b) -> Reader r b
+  (>>=) (Reader f) g = Reader $ \r -> runReader (g (f r)) r
+
+
+
 
 main :: IO ()
 main = do
